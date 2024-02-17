d5a696766ae4f0156c3ea499f85150f9015eef62
roger <rogerwu63@gmail.com>
2024-02-17 04:27:13 -0700

    correct .kv file syntax

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ kivy.require('1.9.0') # Kivy ver where the code has been tested
 from kivy.app import App
 from kivy.uix.label import Label
 
-class MainApp(App):
+class e4App(App):
 	def build(self):
-		return Label(ext="Hello World")
+		return Label()
 
 if __name__ == '__main__':
-	MainApp().run()
+	e4App().run()
